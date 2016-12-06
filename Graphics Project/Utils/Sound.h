686c0baea73c3342fc07bdb558e9c169ea1813cb
Omar Wael <omar_bazaraa@hotmail.com>
2016-12-06 07:55:02 +0200

    Added dummy claseses for text and sound

---
+++ Graphics Project/Utils/Sound.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// STL Includes
+#include <iostream>
+using namespace std;
+
+
+/*
+	Defines some variables needed for frame time calculations
+*/
+class Sound
+{
+public:
+	/* Constructor */
+	Sound() {
+
+	}
+
+	/* Destructor */
+	~Sound() {
+
+	}
+};
---
+++ Graphics Project/Utils/Text.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// STL Includes
+#include <iostream>
+using namespace std;
+
+
+/*
+	Defines some variables needed for frame time calculations
+*/
+class Text
+{
+public:
+	/* Constructor */
+	Text() {
+
+	}
+
+	/* Destructor */
+	~Text() {
+
+	}
+};
